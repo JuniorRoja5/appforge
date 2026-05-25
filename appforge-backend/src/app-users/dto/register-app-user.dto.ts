03d2dccab3adff4a77ab473da55cb36871e9b36f
Caverna Tecnologica LLC <cavernatecnologica@gmail.com>
2026-05-25 02:40:09 +0200

    fix(app-users): add class-validator decorators to DTOs -- closes #8

--- appforge-backend/src/app-users/dto/register-app-user.dto.ts
+++ appforge-backend/src/app-users/dto/register-app-user.dto.ts
@@ -1,6 +1,21 @@
+import { IsEmail, IsString, MinLength, MaxLength, IsOptional } from 'class-validator';
+
 export class RegisterAppUserDto {
+  @IsEmail()
   email!: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(128)
   password!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
   firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
   lastName?: string;
 }
--- appforge-backend/src/app-users/dto/update-app-user.dto.ts
+++ appforge-backend/src/app-users/dto/update-app-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsOptional, IsString, IsUrl, MaxLength } from 'class-validator';
+
 export class UpdateAppUserDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
   firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
   lastName?: string;
+
+  @IsOptional()
+  @IsUrl()
+  @MaxLength(512)
   avatarUrl?: string;
 }
